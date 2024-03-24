5e20fc207d11ac0663cf8804778f8566eee46a9b
bhupen06 <164058624+bhupen06@users.noreply.github.com>
2024-03-25 01:29:25 +0530

    Update SimpleInterest.c
    float to int

--- SimpleInterest.c
+++ SimpleInterest.c
@@ -3,13 +3,14 @@
 #include<conio.h>
 void main()
          {
-             float p,t,r,si;          // P = Principal, R = Rate, T = Time, si=simple interest
+             int p,t,r;
+             float si;          // P = Principal, R = Rate, T = Time, si=simple interest
              printf("Enter principal value :");
-             scanf("%f",&p);
+             scanf("%d",&p);
              printf("Enter rate value :");
-             scanf("%f",&r);
+             scanf("%d",&r);
              printf("Enter time value :");
-             scanf("%f",&t);
+             scanf("%d",&t);
              si=(p*r*t)/100;
              printf("your principal is =%.2f",si);
              getch();
